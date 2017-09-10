08120e195777c770e7cee2dda606896e8aad5450
bern de leon <berndeleon@berns-MacBook-Air.local>
2017-09-10 14:16:03 +0800

    Initial Commit

---
+++ PokeDex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeDex3
+//
+//  Created by bern de leon on 9/10/17.
+//  Copyright © 2017 bern de leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
